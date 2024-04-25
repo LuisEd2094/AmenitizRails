f8e8725ca3f5ee59864b948cea5e68027848ed14
Luis <luissoto2094@gmail.com>
2024-04-25 11:03:44 +0200

    added new promotions and products to handle them. 3x1, 3x2 and 1/5 promo after third unit

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,25 @@
 #   end
 
 Promotion.create!(name:"2x1",condition: 2, discount_type: "EQUAL", divisor: 1.00)
+Promotion.create!(name:"3x2",condition: 3, discount_type: "EQUAL", divisor: 1.00)
+Promotion.create!(name:"3x1",condition: 3, discount_type: "EQUAL", divisor: 0.50)
+
 Promotion.create!(name:"10% after 3 units",condition: 3, discount_type: "GREATER", divisor: 10.00)
 Promotion.create!(name:"33.33% after 3 units",condition: 3, discount_type: "GREATER", divisor: 3.00)
+Promotion.create!(name:"1/5 after 3 units",condition: 3, discount_type: "GREATER", divisor: 5.00)
 
 promotion_2x1 = Promotion.find_by(name: "2x1")
 promotion_10percent = Promotion.find_by(name: "10% after 3 units")
 promotion_33_percent = Promotion.find_by(name: "33.33% after 3 units")
 
+promotion_3x2 = Promotion.find_by(name: "3x2")
+promotion_3x1 = Promotion.find_by(name: "3x1")
+promotion_a_fifth = Promotion.find_by(name: "1/5 after 3 units")
+
 Product.create!(code: "GR1", name: "Green Tea", price: 3.11, promotion: promotion_2x1)
 Product.create!(code: "SR1", name: "Strawberries", price: 5.00, promotion: promotion_10percent)
 Product.create!(code: "CF1", name: "Coffee", price: 11.23, promotion: promotion_33_percent)
 Product.create!(code: "CF2", name: "Coffee No Promo", price: 11.23)
+Product.create!(code: "CF3", name: "Coffee_3x2", price: 11.23, promotion: promotion_3x2)
+Product.create!(code: "CF4", name: "Coffee_3x1", price: 11.23, promotion: promotion_3x1)
+Product.create!(code: "CF5", name: "Coffee_fifth", price: 11.23, promotion: promotion_a_fifth)
