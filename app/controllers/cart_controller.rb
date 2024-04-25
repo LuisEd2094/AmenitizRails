526d5d4bcb1d1857e76245b0d4feef0af41928e1
Luis <luissoto2094@gmail.com>
2024-04-25 11:02:30 +0200

    added more tests to controller to check correct values with more promotions. Modified the formula to return the values to use regx to be more precise as it was losing precision in some cases

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,26 +4,54 @@ class CartController < ApplicationController
         @products = Product.all
         @total = 0
     end
-    
-    def add_to_cart 
-        puts params
-        begin
-            product = Product.get_product(code: params[:products][0][:productCode])
-        rescue ActiveRecord::RecordNotFound => e
-            render json: { message: "Product not found", error: "Product not found" }, status: :not_found
-            return
+
+    def fill_cart (products)
+
+    end
+    def add_to_cart
+        cart = {}
+        products = params[:cart][:products]
+        products.each do | product |
+            begin
+                new_product= Product.get_product(code: product[:product_code])
+                cart[new_product.id] = {product: new_product, quantity: product[:quantity]}
+            rescue ActiveRecord::RecordNotFound => e
+                render json: { message: "Product not found", error: "Product not found" }, status: :not_found
+                return
+            rescue => e
+                render json: {error: e}
+                return
+            end
         end
 
 
-        has_discount = true
-        discount_amount = 3.11
-        total = 3.11
+        has_discount = false
+        total = BigDecimal(0.00)
+        total_discount_amount = BigDecimal(0.00)
+
+        cart.each do | key, value |
+            discount_amount = BigDecimal(0.00)
+            product = value[:product]
+            quantity = value[:quantity]
+            promotion = Promotion.get_promotion(id: product.promotion_id)
+            if promotion
+                discount_amount = promotion.get_discount(quantity, product.price)
+                total_discount_amount += discount_amount
+            end
+            total += (product.price * quantity) - discount_amount
+        end
+        if total_discount_amount > 0.00
+            has_discount = true
+        end
+
+        #.to_f.truncate(2).to_s so that it can return just up to the second decimal and we get the correct expected results
+        
         render json: { 
             message: "Item added to cart successfully", 
             success: true, 
             has_discount: has_discount, 
-            discount_amount: discount_amount, 
-            total: total 
+            discount_amount: total_discount_amount.to_f.to_s.sub(/(\d+\.\d{2})\d*/, '\1'), 
+            total: total.to_f.to_s.sub(/(\d+\.\d{2})\d*/, '\1')
         }
 
     end
--- test/controllers/cart_controller_test.rb
+++ test/controllers/cart_controller_test.rb
@@ -5,14 +5,14 @@ class CartControllerTest < ActionDispatch::IntegrationTest
   test "should add product to cart with valid product" do
     product = products(:one) 
     
-    post "/add_to_cart", params: { product_code: product.code, quantity: 2 }, as: :json
+    post "/add_to_cart", params: {cart: {products: [{product_code: product.code, quantity: 2}]}}, as: :json
     
     assert_response :success
     #assert_includes response.parsed_body["cart"]["items"], { "product_code" => product.id, "quantity" => 2 }
   end
   
   test "should return error with invalid product" do
-    post "/add_to_cart", params: { product_code: 999, quantity: 2 }, as: :json
+    post "/add_to_cart", params: {cart: {products: [{ product_code: "aaa", quantity: 2 }]}}, as: :json
     
     assert_response :not_found
     #assert_equal "Product not found", response.parsed_body["error"]
@@ -22,7 +22,7 @@ class CartControllerTest < ActionDispatch::IntegrationTest
   test "validate if the prices in response are correct" do
     product = products(:one) 
     
-    post "/add_to_cart", params: { product_code: product.code, quantity: 2 }, as: :json
+    post "/add_to_cart", params: {cart: {products: [{product_code: product.code, quantity: 2}]} }, as: :json
     assert_response :success
 
     response_json = JSON.parse(response.body)
@@ -37,8 +37,290 @@ class CartControllerTest < ActionDispatch::IntegrationTest
     expected_discount_amount = product.price
     expected_total = product.price
     assert_equal expected_has_discount, response_json["has_discount"]
-    assert_equal expected_discount_amount, response_json["discount_amount"]
-    assert_equal expected_total, response_json["total"]
+    assert_equal expected_discount_amount.to_s, response_json["discount_amount"]
+    assert_equal expected_total.to_s, response_json["total"]
 
   end
+
+  def assert_prices(expected_total, expected_discount, expected_has_discount, params)
+    post "/add_to_cart", params: params, as: :json
+
+    assert_response :success
+    response_json = JSON.parse(response.body)
+    assert_equal expected_has_discount, response_json["has_discount"]
+    assert_equal expected_discount.to_s, response_json["discount_amount"]
+    assert_equal expected_total.to_s, response_json["total"]
+  end
+
+
+  test "2x1" do
+    params = {cart: {
+        products: [{product_code: "GR1", quantity: 1}]
+      }
+    }
+    assert_prices(products(:one).price, 0.00, false, params)
+
+    params = {cart: {
+      products: [{product_code: "GR1", quantity: 2}]
+      }
+    }
+    assert_prices(products(:one).price, products(:one).price, true, params)
+
+    params = {cart: {
+      products: [{product_code: "GR1", quantity: 3}]
+      }
+    }
+    assert_prices(6.22, products(:one).price, true, params)
+
+    params = {cart: {
+      products: [{product_code: "GR1", quantity: 4}]
+      }
+    }
+    assert_prices(6.22, 6.22, true, params)
+
+    params = {cart: {
+      products: [{product_code: "GR1", quantity: 5}]
+      }
+    }
+    assert_prices(9.33, 6.22,  true, params)
+
+    params = {cart: {
+      products: [{product_code: "GR1", quantity: 6}]
+      }
+    }
+    assert_prices(9.33, 9.33, true, params)
+
+    params = {cart: {
+      products: [{product_code: "GR1", quantity: 7}]
+      }
+    }
+    assert_prices(12.44, 9.33, true, params)
+
+    params = {cart: {
+      products: [{product_code: "GR1", quantity: 9999}]
+      }
+    }
+    assert_prices(15550.0, 15546.89, true, params)
+
+  end
+
+  test "10% discount after 3rd unit" do
+    params = {cart: {
+      products: [{product_code: "SR1", quantity: 1}]
+      }
+    }
+    assert_prices(5.00, 0.0, false, params)
+
+    params = {cart: {
+      products: [{product_code: "SR1", quantity: 2}]
+      }
+    }
+    assert_prices(10.00, 0.0, false, params)
+
+
+    params = {cart: {
+      products: [{product_code: "SR1", quantity: 3}]
+      }
+    }
+    assert_prices(13.5, 1.5, true, params)
+
+
+    params = {cart: {
+      products: [{product_code: "SR1", quantity: 4}]
+      }
+    }
+    assert_prices(18.0, 2.0 , true, params)
+
+
+    params = {cart: {
+      products: [{product_code: "SR1", quantity: 5}]
+      }
+    }
+    assert_prices(22.5, 2.5 , true, params)
+
+    params = {cart: {
+      products: [{product_code: "SR1", quantity: 9999}]
+      }
+    }
+    assert_prices(44995.5, 4999.5, true, params)
+
+  
+  end
+
+  
+  test "2/3 discount after 3rd unit" do
+    params = {cart: {
+      products: [{product_code: "CF1", quantity: 1}]
+      }
+    }
+    assert_prices(11.23, 0.0, false, params)
+
+    params = {cart: {
+      products: [{product_code: "CF1", quantity: 2}]
+      }
+    }
+    assert_prices(22.46, 0.0, false, params)
+
+
+    params = {cart: {
+      products: [{product_code: "CF1", quantity: 3}]
+      }
+    }
+    assert_prices(22.46, 11.23, true, params)
+
+
+    params = {cart: {
+      products: [{product_code: "CF1", quantity: 4}]
+      }
+    }
+    assert_prices(29.94, 14.97 , true, params)
+
+
+    params = {cart: {
+      products: [{product_code: "CF1", quantity: 5}]
+      }
+    }
+    assert_prices(37.43, 18.71 , true, params)
+
+    params = {cart: {
+      products: [{product_code: "CF1", quantity: 9999}]
+      }
+    }
+    assert_prices(74859.18, 37429.59, true, params)
+
+  
+  end
+
+  test "3x2" do
+    params = {cart: {
+      products: [{product_code: "CF3", quantity: 2}]
+      }
+    }
+    assert_prices(22.46, 0.00, false, params)
+
+
+    params = {cart: {
+      products: [{product_code: "CF3", quantity: 3}]
+      }
+    }
+    assert_prices(22.46, 11.23, true, params)
+
+    params = {cart: {
+      products: [{product_code: "CF3", quantity: 9999}]
+      }
+    }
+    assert_prices(74859.18, 37429.59, true, params)
+  end
+
+  test "3x1" do
+    params = {cart: {
+      products: [{product_code: "CF4", quantity: 2}]
+      }
+    }
+    assert_prices(22.46, 0.00, false, params)
+
+
+    params = {cart: {
+      products: [{product_code: "CF4", quantity: 3}]
+      }
+    }
+    assert_prices(11.23, 22.46, true, params)
+
+    params = {cart: {
+      products: [{product_code: "CF4", quantity: 9999}]
+      }
+    }
+    assert_prices(37429.59, 74859.18, true, params)
+  end
+
+  test "Coffee_fifth" do
+    params = {cart: {
+      products: [{product_code: "CF5", quantity: 2}]
+      }
+    }
+    assert_prices(22.46, 0.00, false, params)
+
+
+    params = {cart: {
+      products: [{product_code: "CF5", quantity: 3}]
+      }
+    }
+    assert_prices(26.95, 6.73, true, params)
+
+    params = {cart: {
+      products: [{product_code: "CF5", quantity: 9999}]
+      }
+    }
+    assert_prices(89831.01, 22457.75, true, params)
+  end
+
+  test "first file example" do
+    params = {cart: {
+        products: [{product_code: "GR1", quantity: 2}]
+      }
+    }
+    assert_prices(products(:one).price, products(:one).price, true, params)
+  end
+
+
+  test "Second file example" do
+    params = {cart: {
+        products: [{product_code: "GR1", quantity: 1}, 
+        {product_code: "SR1", quantity: 3}]
+      }
+    }
+    assert_prices(16.61, 1.5, true, params)
+  end
+
+
+
+  test "Third file example" do
+    params = {cart: {
+        products: [{product_code: "GR1", quantity: 1}, 
+        {product_code: "SR1", quantity: 1},
+        {product_code: "CF1", quantity: 3},
+      ]
+      }
+    }
+    assert_prices(30.57, 11.23, true, params)
+  end
+
+  test "first file example + product without promotion" do
+    params = {cart: {
+        products: [{product_code: "GR1", quantity: 2},
+        {product_code: "CF2", quantity: 1},
+      ]
+      }
+    }
+    assert_prices(14.34, products(:one).price, true, params)
+  end
+
+
+  test "Second file example  + product without promotion" do
+    params = {cart: {
+        products: [{product_code: "GR1", quantity: 1}, 
+        {product_code: "SR1", quantity: 3},
+        {product_code: "CF2", quantity: 1}
+      ]
+      }
+    }
+    assert_prices(27.84, 1.5, true, params)
+  end
+
+  test "Third file example + product without promotion" do
+    params = {cart: {
+        products: [{product_code: "GR1", quantity: 1}, 
+        {product_code: "SR1", quantity: 1},
+        {product_code: "CF1", quantity: 3},
+        {product_code: "CF2", quantity: 1}
+      ]
+      }
+    }
+    assert_prices(41.8, 11.23, true, params)
+  end
+
 end
+
+
+
+
