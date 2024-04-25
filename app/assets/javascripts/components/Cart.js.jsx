9ac39da71748e6120a57a13720d1725ad21bbdff
Luis <luissoto2094@gmail.com>
2024-04-25 16:47:23 +0200

    Saving balanced state. It can send and receive information and display it correctly. Need to config jest to be able to test components

---
+++ app/assets/javascripts/components/Cart.js.jsx
@@ -0,0 +1,139 @@
+import { Container } from 'react-bootstrap';
+
+
+const productGridStyles = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+  gap: '20px',
+};
+
+const productCardStyles = {
+  border: '1px solid #ccc',
+  padding: '20px',
+};
+
+function Cart (props) {
+  const [selectedProducts, setSelectedProducts] = React.useState([]);
+  const [total, setTotal] = React.useState(null);
+  const [discount, setDiscount] = React.useState(null);
+  const [hasDiscount, setHasDiscount] = React.useState(null);
+
+
+  const handleAddToCart = (product_code, quantity) => {
+    const existingProductIndex = selectedProducts.findIndex(product => product.product_code === product_code);
+
+    if (existingProductIndex !== -1) {
+      // If the product exists, update its quantity
+      setSelectedProducts(prevSelectedProducts => {
+        const updatedProducts = [...prevSelectedProducts];
+        updatedProducts[existingProductIndex].quantity = quantity;
+        return updatedProducts;
+      });
+    } else {
+      // If the product doesn't exist, add it to selectedProducts
+      setSelectedProducts(prevSelectedProducts => [...prevSelectedProducts, { product_code, quantity }]);
+    }
+  };
+
+  const getQuantity = (product_code) => 
+  {    
+    const existingProductIndex = selectedProducts.findIndex(product => product.product_code === product_code);
+
+    if (existingProductIndex !== -1) 
+    {
+        return selectedProducts[existingProductIndex].quantity;
+    }
+    else {
+      // If the product doesn't exist, add it to selectedProducts
+      return 0
+    }
+  }
+
+
+  const handleIncrement = (product_code) => {
+    const existingProductIndex = selectedProducts.findIndex(product => product.product_code === product_code);
+
+    if (existingProductIndex !== -1)
+    {    
+      const updatedProducts = [...selectedProducts];
+      handleAddToCart(product_code, updatedProducts[existingProductIndex].quantity + 1 )
+      console.log([...selectedProducts])
+    }
+    else
+    {
+      handleAddToCart(product_code, 1);
+    }
+  };
+
+  const handleDecrement = (product_code) => {
+    const existingProductIndex = selectedProducts.findIndex(product => product.product_code === product_code);
+
+    if (existingProductIndex !== -1)
+    {    
+      const updatedProducts = [...selectedProducts];
+      if (updatedProducts[existingProductIndex].quantity > 1)
+        handleAddToCart(product_code, updatedProducts[existingProductIndex].quantity - 1 )
+      else 
+        handleAddToCart(product_code, 0 )
+      console.log([...selectedProducts])
+    }
+    else
+    {
+      handleAddToCart(product_code, 0);
+    }
+  };
+
+
+  
+  const handleSubmitPurchase = (event) => {
+    event.preventDefault();
+    const csrfToken = document.querySelector('[name=csrf-token]').content;
+    const response = fetch('/add_to_cart', 
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrfToken,
+
+      },
+      body: JSON.stringify({ products: selectedProducts }),
+    }
+    );
+
+    response.then(response => {
+      if (!response.ok) 
+      {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.has_discount)
+      {
+        setHasDiscount(true);
+        setDiscount(data.discount_amount)
+      }
+      setTotal(data.total);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+  }
+
+  return(
+    <div>
+      <ProductList getQuantity={getQuantity} cart={selectedProducts} products={props.products} handleAddToCart={handleAddToCart}  handleIncrement={handleIncrement} handleDecrement={handleDecrement}></ProductList>
+      <button onClick={handleSubmitPurchase}>Add to Cart</button>
+
+      {total && (
+      <div>
+        <h3>Total: {total}€</h3>
+      </div>
+      )}
+      {hasDiscount && (
+          <div> 
+            <h3>Discount: {discount}€</h3>
+          </div>
+      )
+    }</div>)
+};
---
+++ app/assets/javascripts/components/ProductList.js.jsx
@@ -0,0 +1,35 @@
+
+const productGridStyles = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+  gap: '20px',
+
+};
+
+const productCardStyles = {
+  border: '1px solid #ccc',
+  padding: '20px',
+};
+
+
+
+function ProductList({ products, handleAddToCart, handleIncrement, handleDecrement, getQuantity }){
+  return (
+    <div style={productGridStyles}>
+      {products.map(product => (
+        <div key={product.id} style={productCardStyles}>
+          <div>
+            <h3>{product.name}</h3>
+            <p>Price: ${product.price}€</p>
+          </div>
+          <input type="number" min="1" defaultValue="0" onChange={(e) => handleAddToCart(product.code, parseInt(e.target.value))} />
+          <div>
+              <button onClick={() => handleDecrement(product.code)}>-</button>
+              <span style={{ margin: '0 10px' }}>{getQuantity(product.code)}</span>
+              <button onClick={() => handleIncrement(product.code)}>+</button>
+            </div>
+        </div>
+      ))}
+    </div>
+  );
+};
